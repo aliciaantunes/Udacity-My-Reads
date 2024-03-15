4b799ad8d15eb46219c299dc58de1b372c68f77a
Alícia Antunes <antunesalicia07@gmail.com>
2024-03-15 16:06:02 +0000

    feat: paste html

---
+++ src/components/Pages/BookSearch.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import PropTypes from 'prop-types';
+
+
+
+
+
+
+
+return (
+<div className="search-books">
+<div className="search-books-bar">
+  <button
+    className="close-search"
+    onClick={() => setShowSearchpage(!showSearchPage)}
+  >
+    Close
+  </button>
+  <div className="search-books-input-wrapper">
+    <input
+      type="text"
+      placeholder="Search by title, author, or ISBN"
+    />
+  </div>
+</div>
+<div className="search-books-results">
+  <ol className="books-grid"></ol>
+</div>
+</div>
+);
