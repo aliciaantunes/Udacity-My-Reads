8c55d7e80e134ec25096cef9f4550d1d78c3261e
Alícia Antunes <antunesalicia07@gmail.com>
2024-03-16 17:49:16 +0000

    feat: change the url of books

--- src/components/common/Book.jsx
+++ src/components/common/Book.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PropTypes } from "prop-types";
+import PropTypes from "prop-types";
 
 function Book ({book, onChangeShelf}) {
     const ChangeShelf = (e) => {
@@ -17,7 +17,7 @@ function Book ({book, onChangeShelf}) {
              width: 128,
              height: 193,
              backgroundImage:
-             'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+             `url("${book.imageLinks && book.imageLinks.thumbnail}")`,
             }}
              ></div>
             <div className="book-shelf-changer">
