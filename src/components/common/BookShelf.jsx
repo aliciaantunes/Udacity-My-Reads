a3de79aa4e6a3670ef4201e377597cd578a19795
Alícia Antunes <antunesalicia07@gmail.com>
2024-03-16 14:48:35 +0000

    paste the html

--- src/components/common/BookShelf.jsx
+++ src/components/common/BookShelf.jsx
@@ -1,8 +1,15 @@
-
+import React from "react";
 
 function Bookshelf({books, title, changeBookShelf}) {
 
-
+return (
+    <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                 <div className="bookshelf-books">
+                  <ol className="books-grid"></ol>
+        </div>
+        </div>
+)
 }
 
 
