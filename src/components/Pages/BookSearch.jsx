3d0103ac6cc995d3f7715045457f009d9971ced8
Alícia Antunes <antunesalicia07@gmail.com>
2024-03-16 18:08:45 +0000

    feat: add debounce

--- src/components/Pages/BookSearch.jsx
+++ src/components/Pages/BookSearch.jsx
@@ -1,9 +1,32 @@
 import { useState } from "react";
-
+import { debounce } from 'throttle-debounce';
+import * as BooksAPI from '../../utils/BooksAPI';
 
 function BookSearch({myBooks, changeBooksShelf}) {
     const [books, setBooks] = useState([]);
     const [searchParameter, setSearchParameter] = useState('');
 
-     
+    const searchBooks = debounce(400, (searchParameter) => {
+       if (searchParameter.length > 0) {
+        BooksAPI.search(searchParameter, 10)
+           .then((response) => {
+             if (response.length > 0) {
+                  setBooks(response);
+             } else {
+              setBooks([]);
+             }
+           });
+       } else {
+            setBooks([]);
+       }
+    });
+
+
+
+
+
+
+
 }
+
+export default BookSearch;
