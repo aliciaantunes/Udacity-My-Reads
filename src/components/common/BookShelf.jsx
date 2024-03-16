c5d121d79a15886fc9c696c221d21a01f84277f4
Alícia Antunes <antunesalicia07@gmail.com>
2024-03-16 15:07:26 +0000

    feat: changeshelf map

--- src/components/common/BookShelf.jsx
+++ src/components/common/BookShelf.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import Book from './Book';
 
 function Bookshelf({books, title, changeBookShelf}) {
 
 return (
-    <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                 <div className="bookshelf-books">
-                  <ol className="books-grid"></ol>
-        </div>
-        </div>
-)
-}
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{title}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+               {books.map(book => ( 
+                 <Book key={book.id} 
+                 book={book} 
+                 onChangeShelf={changeBookShelf} />
+                ))}
+              </ol>
+            </div>
+         </div>
+      );
+    }
 
 
 
