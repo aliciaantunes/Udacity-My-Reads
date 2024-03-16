f0b2767e609df80a5eb3bc25cb63a5dd631638f2
Alícia Antunes <antunesalicia07@gmail.com>
2024-03-16 15:45:41 +0000

    feat: create filterlist

--- src/components/Pages/BookList.jsx
+++ src/components/Pages/BookList.jsx
@@ -1,11 +1,28 @@
-
+import React from 'react';
+import Bookshelf from '../common/BookShelf';
 
 
 function BookList({myBooks, changeBookShelf}){
   
     const filterBooks = (shelf) => {
       return myBooks.filter(book => (book.shelf === shelf));
-    }
+  }
+     
+     return (
+        <div className="list-books">
+           <div className="list-books-title">
+             <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <Bookshelf books={filterBooks('currentlyReading')} title= "Currently Reading" changeBookShelf={changeBookShelf} />
+            <Bookshelf books={filterBooks('wantToRead')} title="Want to Read" changeBookShelf={changeBookShelf} />
+            <Bookshelf books={filterBooks('read')} title="Read" changeBookShelf={changeBookShelf} />
+            <div>
+      </div>
+      </div>
+      </div>               
+     )
+
 };
 
 export default BookList;
