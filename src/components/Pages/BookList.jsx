9495f622103159097030cccc5de0fddac871c7e3
Alícia Antunes <antunesalicia07@gmail.com>
2024-03-17 10:59:48 +0000

    add search button

--- src/components/Pages/BookList.jsx
+++ src/components/Pages/BookList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Bookshelf from '../common/BookShelf';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 
 function BookList({myBooks, changeBookShelf}){
@@ -18,6 +19,9 @@ function BookList({myBooks, changeBookShelf}){
             <Bookshelf books={filterBooks('currentlyReading')} title= "Currently Reading" changeBookShelf={changeBookShelf} />
             <Bookshelf books={filterBooks('wantToRead')} title="Want to Read" changeBookShelf={changeBookShelf} />
             <Bookshelf books={filterBooks('read')} title="Read" changeBookShelf={changeBookShelf} />
+            <div className="open-search">
+              <Link to="/search">Add a book</Link>
+          </div>
       </div>
       </div>               
      );
